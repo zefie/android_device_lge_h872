8183b5e5947fb079b90da88849598c712c32d0e8
Armando Partida <armandopartida0@gmail.com>
2019-06-26 10:43:44 -0700

    Initial LG G6 H872 Orangefox tree
    * Still working on it

---
+++ Android.mk
@@ -0,0 +1,7 @@
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),h872)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(CLEAR_VARS)
+endif
---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_h872.mk
